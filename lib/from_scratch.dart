49d1c9d67ef8aebad725e27a5c4613d3b98bec6b
Norbert <norbertkozsir@gmail.com>
2018-03-27 11:21:21 +0200

    rewritten to use one expanding item

--- lib/from_scratch.dart
+++ lib/from_scratch.dart
@@ -27,11 +27,11 @@ class MyAppState extends State<MyApp2> {
     ..add(new Data('0'))
     ..add(new Data('1'))
     ..add(new Data('2'))
- //   ..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
-   // ..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
     ..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
+    ..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
+   // ..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
     //..add(new Data('3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsd3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsf3ffffffffffffffffffsdfsdfsdfsdfsdfsdfsdfsdfsdfdsffsdfsdfsdfsdfsdfsdfdsf'))
-    //..add(new Data('3'))
+    ..add(new Data('3'))
     ..add(new Data('4'))
     ..add(new Data('5'));
   /*  ..add('7')
@@ -102,29 +102,45 @@ class MyAppState extends State<MyApp2> {
                 print("drag started at $index");
                 setState((){
                   rows.removeAt(index);
-                  rows.removeAt(index);
-              //    rows[index].color = Colors.white;
-                  rows.insert(index, new Data(""));
                 });
               },
               onDragCompleted: (){
                 shouldScrollUp = false;
+                _currentIndex = null;
+                _currenScrollPos = 0.0;
+                _currentMiddle = null;
+                _dataAndOffset = null;
               },
               onAccept: (Data data) {
                 //data is row coming from
+
                 setState((){
-                  rows.removeAt(index);
-                  rows.insert(index, data);
+                  if(_currentMiddle.dy > _currenScrollPos) {
+                    rows[index].extraTop = 0.0;
+                    rows[index].extraBot = 0.0;
+                    rows.insert(index, data);
+                    rows[index].extraTop = 0.0;
+                    rows[index].extraBot = 0.0;
+                  } else {
+                    rows[index].extraTop = 0.0;
+                    rows[index].extraBot = 0.0;
+                    rows.insert(index + 1, data);
+                    rows[index + 1].extraTop = 0.0;
+                    rows[index + 1].extraBot = 0.0;
+                  }
                 });
 
               },
               onLeave: (DataAndOffset df) {
                 Data data = df.data;
                 Offset offset = df.offset;
-                if(rows[index].data != "") return;
                 // Debug
+             //   print('$index leaving');
+
                 setState((){
-                    rows.removeAt(index);
+                   // rows.removeAt(index);
+                  rows[index].extraBot = 0.0;
+                  rows[index].extraTop = 0.0;
                 });
 
               },
@@ -132,10 +148,20 @@ class MyAppState extends State<MyApp2> {
                 Data data = dataAndOffset.data;
                 Offset offset = dataAndOffset.offset;
 
-             //   print('$index will accept row ${data.data}');
-                print("$index and $offset scrollpos $_currenScrollPos");
+                _currentIndex = index;
+                _dataAndOffset = dataAndOffset.size;
+                _currentMiddle = offset;
 
+              //  print('$index will accept row ${data.data}');
+            //    print("$index and $offset scrollpos $_currenScrollPos");
+
+                print('Middle: ${offset.dy} curren scrool pos: $_currenScrollPos');
                 setState((){
+                  if(dataAndOffset.offset.dy > _currenScrollPos) {
+                    rows[index].extraTop = dataAndOffset.size.height;
+                  } else {
+                    rows[index].extraBot = dataAndOffset.size.height;
+                  }
                     //rows.insert(index, new Data(""));
                 });
 
@@ -143,6 +169,7 @@ class MyAppState extends State<MyApp2> {
               },
               onMove: (Offset offset){
                 _currenScrollPos = offset.dy;
+                _maybeChange();
                 double screenHeight = MediaQuery.of(context2).size.height;
 
                 if(offset.dy < _kScrollThreashhold) {
@@ -167,6 +194,23 @@ class MyAppState extends State<MyApp2> {
 
   }
 
+  Offset _currentMiddle;
+  Size _dataAndOffset;
+  int _currentIndex;
+  void _maybeChange() {
+    if(_currentMiddle == null || _dataAndOffset == null ||_currenScrollPos == null || _currentIndex == null) return;
+    setState((){
+      if(_currentMiddle.dy > _currenScrollPos) {
+        rows[_currentIndex].extraTop = _dataAndOffset.height;
+        rows[_currentIndex].extraBot = 0.0;
+      } else {
+        rows[_currentIndex].extraBot = _dataAndOffset.height;
+        rows[_currentIndex].extraTop = 0.0;
+      }
+      //rows.insert(index, new Data(""));
+    });
+  }
+
 }
 
 class DraggableListItem extends StatelessWidget {
@@ -183,9 +227,6 @@ class DraggableListItem extends StatelessWidget {
 
   final double draggedHeight;
 
-  double extraTop;
-  double extraBot;
-
 
   final ValueChanged<double> onDragStarted;
   final VoidCallback onDragCompleted;
@@ -195,7 +236,7 @@ class DraggableListItem extends StatelessWidget {
   final ValueChanged<Offset> onMove;
 
   DraggableListItem({this.data, this.index, this.onDragStarted, this.onDragCompleted, this.onAccept, this.onLeave, this.onWillAccept,
-    this.onMove, this.draggedHeight, this.extraTop = 0.0, this.extraBot = 0.0, });
+    this.onMove, this.draggedHeight});
 
   @override
   Widget build(BuildContext context) {
@@ -227,21 +268,24 @@ class DraggableListItem extends StatelessWidget {
 
   Widget _getListChild(int index, BuildContext context) {
     return new MyDragTarget<Data>(builder: (BuildContext context, List candidateData, List rejectedData) {
-      if(data.data == "") {
-        return new SizedBox(height: draggedHeight,);
-      }
-      return _getActualChild();
+      return new Column(
+        children: <Widget>[
+          new SizedBox(height: data.extraTop,),
+          _getActualChild(),
+          new SizedBox(height: data.extraBot,),
+        ],
+      );
     },
       onAccept: onAccept,
       onLeave: (dAndF){
         RenderBox it = context.findRenderObject() as RenderBox;
         var offset = it.localToGlobal(new Offset(0.0, it.size.height / 2));
-        return onLeave(new DataAndOffset(data, offset));
+        return onLeave(new DataAndOffset(data, offset, it.size));
       },
       onWillAccept: (data){
         RenderBox it = context.findRenderObject() as RenderBox;
         var offset = it.localToGlobal(new Offset(0.0, it.size.height / 2));
-        return onWillAccept(new DataAndOffset(data, offset));
+        return onWillAccept(new DataAndOffset(data, offset, it.size));
       },
 
     );
@@ -264,12 +308,17 @@ class Data {
   final String data;
   Color color = Colors.white;
 
-  Data(this.data, {this.color});
+
+  double extraTop;
+  double extraBot;
+
+  Data(this.data, {this.color, this.extraTop = 0.0, this.extraBot = 0.0});
 }
 
 class DataAndOffset{
   final Data data;
   final Offset offset;
+  final Size size;
 
-  DataAndOffset(this.data, this.offset);
+  DataAndOffset(this.data, this.offset, this.size);
 }
